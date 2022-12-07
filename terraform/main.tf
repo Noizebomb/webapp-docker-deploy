e60b54378ec6d6145bb6d0604d254cbc1fdd7726
noizebomb <myemailis319@gmail.com>
2022-12-07 18:36:19 -0500

    added main tf file

---
+++ terraform/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+variable "awsprops" {
+    type = map
+    default = {
+    region = "us-west-2"
+    ami = "ami-0fd3231344475acf9"
+    itype = "t2.micro"
+    vpc = "vpc-0e8a51820ff9f00ac"
+    subnet = "subnet-0ce5f4511388b765b"
+    publicip = true
+    keyname = "oregon-keypair"
+    secgroupname = "tf-sec-grp"
+  }
+}
+
+resource "aws_security_group" "tf-sec-grp" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 443 Transport
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "terraform-project" {
+  count = 2
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet")
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+  vpc_security_group_ids = [
+    aws_security_group.tf-sec-grp.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 30
+    volume_type = "gp2"
+  }
+  tags = {
+    Name = "LVLUP-PROJECT ${count.index}"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.tf-sec-grp ]
+}
+
+
+output "ec2instance" {
+  value = aws_instance.terraform-project.*.public_ip
+}
